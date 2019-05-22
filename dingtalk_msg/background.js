51c64b0e1ed3dae68b09fcf08332c43fa6315c08
open-mfc <mafacai@vip.qq.com>
2019-05-22 21:16:04 +0800

    格式

--- dingtalk_msg/background.js
+++ dingtalk_msg/background.js
@@ -1,15 +1,15 @@
-window.onload = function() {
-  // new Notification('当前浏览器可推送钉钉消息', {
-    // icon: '48.png',
-    // body: '推送成功后，这里会展示名字。请忽略本消息'
-  // });
-  chrome.extension.onRequest.addListener(
-	  function(request, sender, sendResponse) {
-		if(request.type==1){
-			new Notification('推送钉钉消息', {
-				icon: '48.png',
-				body: '接收消息的用户：'+request.name
-			});	
-		}
-	});
+window.onload = function () {
+	// new Notification('当前浏览器可推送钉钉消息', {
+	// icon: '48.png',
+	// body: '推送成功后，这里会展示名字。请忽略本消息'
+	// });
+	chrome.extension.onRequest.addListener(
+		function (request, sender, sendResponse) {
+			if (request.type == 1) {
+				new Notification('推送钉钉消息', {
+					icon: '48.png',
+					body: '接收消息的用户：' + request.name
+				});
+			}
+		});
 }
--- dingtalk_msg/myscript.js
+++ dingtalk_msg/myscript.js
@@ -1,9 +1,6 @@
-var DingUserList = [{ "userid": "01523028338454", "name": "张德良" }, { "userid": "0124470620338", "name": "喻增友" }, { "userid": "manager8700", "name": "李磊" }, { "userid": "01523101226145", "name": "李国增" }, { "userid": "2641043542937967", "name": "王刚" }, { "userid": "100153312536363081", "name": "部国宾" }, { "userid": "0124473115738776471", "name": "帅哥未老" }, { "userid": "054110310924050913", "name": "张会军" }, { "userid": "054110310924050913", "name": "张会军" }, { "userid": "022925500024360016", "name": "张玉龙" }, { "userid": "01523069397077", "name": "马发才" }, { "userid": "01523163375387", "name": "方宁" }, { "userid": "01523245609302", "name": "马金娥" }, { "userid": "263654474622940248", "name": "姚晓嫱" }, { "userid": "013440240321541890", "name": "吴文兵" }, { "userid": "263708081126350599", "name": "景诗鹏" }, { "userid": "122808586039813770", "name": "黄宜波" }, { "userid": "060365422729097899", "name": "王创军" }, { "userid": "100526354124213260", "name": "徐嘉健" }, { "userid": "185720065637560936", "name": "钟祥虎" }, { "userid": "195229170829065417", "name": "王东亚" }, { "userid": "2640323261858255", "name": "杜鑫" }, { "userid": "244860331827112359", "name": "武叶廷" }, { "userid": "01523151117666", "name": "徐泉" }, { "userid": "081009400324452096", "name": "张肖状" }, { "userid": "2636130322781128", "name": "张晨" }, { "userid": "263721044936792995", "name": "郑达成" }, { "userid": "022635025521033475", "name": "刘春杰" }, { "userid": "263715085924253419", "name": "张晨阳" }, { "userid": "0845181618846306", "name": "李杰" }, { "userid": "2637485550779402", "name": "徐刚" }, { "userid": "01523115111197", "name": "王欢" }, { "userid": "013945604524352242", "name": "张瑞杰" }, { "userid": "2640180305843700", "name": "杨娜" }, { "userid": "181434180722936458", "name": "姚显亮" }, { "userid": "0152362753535", "name": "刘慧" }, { "userid": "0105293213846217", "name": "李朗" }, { "userid": "01244663541664", "name": "朱柳" }, { "userid": "012607465735978856", "name": "赵金凤" }, { "userid": "153033570023739617", "name": "宋雪净" }, { "userid": "266812203333697962", "name": "薛旭东" }, { "userid": "015231004021861365", "name": "吕辉辉" }, { "userid": "19444627571143578", "name": "赵凯" }, { "userid": "030851425833400580", "name": "蔡义歌" }, { "userid": "1519612801-436228654", "name": "王万生" }, { "userid": "1057315862905810112", "name": "王建鹏" }, { "userid": "01244847643626", "name": "玍丹凤" }, { "userid": "146425636237616546", "name": "陈丹平" }, { "userid": "042265040235388638", "name": "贾丹丹" }, { "userid": "01423220241180722", "name": "郭睿" }, { "userid": "01244960113618", "name": "徐雁还" }, { "userid": "01244861594422", "name": "喻灵" }, { "userid": "24654131141228381", "name": "闫雨" }, { "userid": "263703254825255237", "name": "接婷婷" }, { "userid": "2738673757684891", "name": "刘艳" }, { "userid": "2560230530787637", "name": "张翕" }, { "userid": "01523039439005", "name": "刘想想" }, { "userid": "01244903099204", "name": "谢菲" }, { "userid": "01522951487154", "name": "黄梁梅" }, { "userid": "02004533579039", "name": "杜颖" }, { "userid": "01522968064180", "name": "朱小萍" }, { "userid": "263700654229073282", "name": "王会云" }, { "userid": "01523758205599", "name": "杨灿" }, { "userid": "215346394130876339", "name": "程梦华" }, { "userid": "01244713339233", "name": "肖小月" }, { "userid": "2020215608789086", "name": "张蕾" }, { "userid": "054914186621419408", "name": "吴可鑫" }, { "userid": "244943274625899350", "name": "易心武" }, { "userid": "674928479497", "name": "杨莹" }, { "userid": "230811283123117814", "name": "孙云华" }, { "userid": "062734276829315279", "name": "王海力" }, { "userid": "272736150629303083", "name": "王欣欣" }, { "userid": "011545018485", "name": "刘耀欢" }, { "userid": "181840155629606071", "name": "王金宝" }, { "userid": "234430453726756383", "name": "樊倚良" }, { "userid": "054962653826138071", "name": "朱学斌" }, { "userid": "6622531229115525", "name": "狄永培" }, { "userid": "261650002629433579", "name": "王笑笑" }, { "userid": "231865080521820032", "name": "喻大双" }, { "userid": "120952615426410756", "name": "梁卫风" }, { "userid": "246139175728725967", "name": "焦海净" }, { "userid": "263822215831132172", "name": "第敏敏" }, { "userid": "261112055221081675", "name": "冯秀东" }, { "userid": "01523146667635", "name": "王立元" }, { "userid": "01523147404606", "name": "闫超" }, { "userid": "01523100179965", "name": "陈佳" }, { "userid": "01523054254826", "name": "李现民" }, { "userid": "263710025526168394", "name": "李宇欣" }, { "userid": "0854551026071137", "name": "曹培鲁" }, { "userid": "182719012933393079", "name": "蔡丽佳" }, { "userid": "246535444029913777", "name": "白家豪" }, { "userid": "1343015633784380", "name": "徐斌" }, { "userid": "225818635326071227", "name": "李东辉" }, { "userid": "2236676868907690", "name": "温芳" }, { "userid": "2258244857775729", "name": "常浩" }, { "userid": "263664460920857802", "name": "刘丽鹏" }, { "userid": "263713485727318510", "name": "樊雯艳" }, { "userid": "26370654651232256", "name": "韦梦" }, { "userid": "09595310011076617", "name": "董斌" }, { "userid": "225824065524167785", "name": "张子龙" }, { "userid": "225826641226120337", "name": "朱妍妍" }, { "userid": "263761405938026118", "name": "韩亚男" }, { "userid": "1421374156687528", "name": "史博" }, { "userid": "176624685030038988", "name": "白海伦" }, { "userid": "1838060200703569", "name": "周莹" }, { "userid": "086343276320471165", "name": "何玉金" }, { "userid": "2637376740686409", "name": "刘衡" }, { "userid": "0124472648121", "name": "闫慧" }, { "userid": "2344242354775830", "name": "张冶" }, { "userid": "092163665823467725", "name": "孙立伟" }, { "userid": "062869602338885305", "name": "马永超" }, { "userid": "152031656226206147", "name": "杨富钧" }, { "userid": "2344516610684951", "name": "刘芯" }, { "userid": "01523054439338", "name": "喻楠" }, { "userid": "264001100424184950", "name": "嵇靖超" }, { "userid": "23530518051289698", "name": "黄琦" }, { "userid": "243511445719983700", "name": "严家祥" }, { "userid": "054456683738751627", "name": "高亚萍" }, { "userid": "2224140429856055", "name": "李超" }, { "userid": "172209200226604678", "name": "杨辉文" }, { "userid": "32122919943098", "name": "王春" }, { "userid": "6739682029129729", "name": "王启超" }, { "userid": "225134080837800472", "name": "陈文婷" }, { "userid": "0152316036638", "name": "陈頔" }, { "userid": "26200763261284143", "name": "黄平" }, { "userid": "183033221823519905", "name": "尹文敏" }, { "userid": "263669485832191723", "name": "罗梓薇" }, { "userid": "2641223038679098", "name": "包伟" }, { "userid": "1250124334843576", "name": "曾晶" }, { "userid": "2734326903956480", "name": "田杰" }, { "userid": "195100494821065061", "name": "包丽丽" }, { "userid": "176811003824176128", "name": "张建亚" }, { "userid": "263833426236218793", "name": "逯宇凡" }, { "userid": "2258246647794034", "name": "张飒" }, { "userid": "225542644929600879", "name": "田常婷" }, { "userid": "032224016621869100", "name": "吕金月" }, { "userid": "1161521734850250", "name": "杨玲" }, { "userid": "26626750321074440", "name": "荆美" }, { "userid": "185405154220758054", "name": "冯伟蝶" }, { "userid": "274106221938666536", "name": "马凯旋" }, { "userid": "264026251123178970", "name": "宋佳欢" }, { "userid": "061845254121189301", "name": "刘秋月" }, { "userid": "1926561944-904998349", "name": "韦彩凤" }, { "userid": "2258240901694358", "name": "吴昊" }, { "userid": "19225034421232039", "name": "韩杰" }, { "userid": "2169573211819688267", "name": "李稳稳" }, { "userid": "2258193501702958", "name": "商晴" }, { "userid": "1755441504688090", "name": "卢曼" }, { "userid": "1153510763774937", "name": "张丹" }, { "userid": "225237641220837571", "name": "刘东升" }, { "userid": "2332452019846025", "name": "李晗" }, { "userid": "222646310637628725", "name": "陈一铭" }, { "userid": "0124471324829", "name": "王尚玲" }, { "userid": "22022058673242", "name": "刘哲" }, { "userid": "173011083228966525", "name": "牛智东" }, { "userid": "20403220421216063", "name": "陈宇" }, { "userid": "33661249673733", "name": "刘囝" }, { "userid": "206219446529225138", "name": "王志飞" }, { "userid": "01103441385706", "name": "宋子娟" }, { "userid": "01523100393596", "name": "周然" }, { "userid": "263939624338003307", "name": "陈红梅" }, { "userid": "103107430929430654", "name": "王秋霞" }, { "userid": "17272046551187408", "name": "郑锁" }, { "userid": "020231046930132242", "name": "盛浏彦" }, { "userid": "114266242523384267", "name": "宋楠楠" }, { "userid": "2620054745753461", "name": "宋楠" }, { "userid": "012507065826560460", "name": "朱述立" }, { "userid": "044324191138283793", "name": "项广婷" }, { "userid": "156728320629400139", "name": "王盼盼" }, { "userid": "263965146832449978", "name": "胡天财" }, { "userid": "204031104329271473", "name": "王新颖" }, { "userid": "5441493821640459", "name": "唐国存" }, { "userid": "26401516161225050", "name": "陈红" }, { "userid": "083210344824346969", "name": "张玲枫" }, { "userid": "263756034620325252", "name": "任洪漳" }, { "userid": "255258486337792408", "name": "闫淑霞" }, { "userid": "2637056061-351654843", "name": "朱婕" }, { "userid": "263802342624246350", "name": "张春芳" }, { "userid": "225820511923537060", "name": "孔艳荣" }, { "userid": "225824485924058925", "name": "张云腾" }, { "userid": "265135006034763348", "name": "裴芳芳" }, { "userid": "265640396529220605", "name": "王思纯" }, { "userid": "150632192820674127", "name": "侯芳芳" }, { "userid": "0138591605781425", "name": "徐威" }, { "userid": "2154444125772223", "name": "师文" }, { "userid": "645523641250178", "name": "高博" }, { "userid": "2537240663844361", "name": "李志" }, { "userid": "2610232116677487", "name": "刘文" }, { "userid": "2262433214680736", "name": "冉莉" }];
+var DingUserList = [{ "userid": "015230283384", "name": "张德良" }];
 
 $(function () {
-	window.showDingResult = function (res) {
-		console('cllback', res);
-	}
 	var StoryOwner = $('#StoryOwner').val();
 	(function () {//等待1秒重新加载处理人
 		if (!StoryOwner) {
@@ -14,42 +11,41 @@ $(function () {
 	})();
 	//编辑工单
 	$('#btn_update_exit').click(function () {
-		sendDingTalk($('#StoryOwner').val(),'编辑');
+		sendDingTalk($('#StoryOwner').val(), '编辑');
 	});
 	//新建需求
 	$('#btn_save_view').click(function () {
-		sendDingTalk($('#StoryOwner').val(),'新建需求');
+		sendDingTalk($('#StoryOwner').val(), '新建需求');
 	});
 	//新建缺陷
 	$('#_view').click(function () {
-		sendDingTalk($('#BugCurrentOwner').val(),'新建缺陷');
+		sendDingTalk($('#BugCurrentOwner').val(), '新建缺陷');
 	});
 	//流转工单
 	$('#update_status_btn').click(function () {
-		sendDingTalk($('#STATUS_planning-planningOwner').val() || $('#STATUS_new-newCurrentOwnerValue').val(),'流转');
+		sendDingTalk($('#STATUS_planning-planningOwner').val() || $('#STATUS_new-newCurrentOwnerValue').val(), '流转');
 	});
-	(function(){
-		function getV()
-		{
-			return $('#ContentStatusOwner .editable-value').text() 
-			|| $('#ContentCurrentOwner .editable-value').text();
+	(function () {
+		function getV() {
+			return $('#ContentStatusOwner .editable-value').text()
+				|| $('#ContentCurrentOwner .editable-value').text();
 			// || $('#owner-field-value .editable-value').prop('title');//内嵌页面无法获取
 		}
-			//已有工单编辑处理人
+		//已有工单编辑处理人
 		var _u = getV();
 		lunxun();
-			function lunxun(){
-				setInterval(function () {
-					var _uuu=getV();
-					if (_uuu && _uuu != _u) {
-						console.log(_u,getV());
-						_u = getV();
-						sendDingTalk(_u || '','变更处理人');
-					}
-				}, 500);
-			}
+		function lunxun() {
+			setInterval(function () {
+				var _uuu = getV();
+				if (_uuu && _uuu != _u) {
+					console.log(_u, getV());
+					_u = getV();
+					sendDingTalk(_u || '', '变更处理人');
+				}
+			}, 500);
+		}
 	})();
-	
+
 	//处理人变更
 	$('#StoryOwnerValue').on('change', function (e) {
 		if (window.location.pathname.indexOf('/edit/') > -1 || window.location.pathname.indexOf('/view/') > -1) {
@@ -60,8 +56,8 @@ $(function () {
 		}
 	});
 	//发送钉钉消息
-	function sendDingTalk(name,t) {
-		console.log('收到通知请求', name,t);
+	function sendDingTalk(name, t) {
+		console.log('收到通知请求', name, t);
 		var ids = [];
 		name.split(';').forEach(function (v) {
 			var ding = $.grep(DingUserList, function (n) { return n.name == v; });
@@ -69,26 +65,14 @@ $(function () {
 				ids.push(ding[0].userid);
 			}
 		});
-		if (ids.length == 0) {console.log('推送人员空'); return false; }
-		var _title = $('#story_name_view .editable-value').text() 
-					|| $('#bug_title_view .editable-value').text() 
-					|| $('.title-edit-wrap .editable-value').text() 
-					|| $('#StoryName').val() 
-					|| $('#BugTitle').val();
+		if (ids.length == 0) { console.log('推送人员空'); return false; }
+		var _title = $('#story_name_view .editable-value').text()
+			|| $('#bug_title_view .editable-value').text()
+			|| $('.title-edit-wrap .editable-value').text()
+			|| $('#StoryName').val()
+			|| $('#BugTitle').val();
 		var _url = "https://o2o.ocpay.cn/v3/vmall.vmall/SendTapdMsg?ids=" + JSON.stringify(ids)
-			+ '&msg=' + encodeURIComponent('['+t+']'+_title);
-		//$("head").append("<script src='" + _url + "'><\/script>");
-		// alert(_url);
-		//发送钉钉消息
-		// $.ajax({
-		// 	url: _url,
-		// 	type: "GET",
-		// 	// dataType: "jsonp", //指定服务器返回的数据类型
-		// 	success: function (data) {
-		// 		//var result = JSON.stringify(data); //json对象转成字符串
-		// 		console.log(data);
-		// 	}
-		// });
+			+ '&msg=' + encodeURIComponent('[' + t + ']' + _title);
 		var xhr = new XMLHttpRequest();
 		xhr.open("GET", _url, true);
 		xhr.onreadystatechange = function () {
